f38d18fb7bd87914fbf618779f9538a33e52236a
jayy734 <angelsoenyunt@gmail.com>
2024-03-20 15:01:07 +0300

    added count letter method

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} was found in this file" )
+    num_letters = count_letter(text)
+    print(f"{num_letters} was found in this file" )
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,17 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letter(text):
+    
+    char = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char:
+            char[lowered] += 1
+        else :
+            char[lowered] = 1
+    return char
+
     
 main()
