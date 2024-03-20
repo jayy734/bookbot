57cc76003228ad663c91ca31da00f561172d30f3
jayy734 <angelsoenyunt@gmail.com>
2024-03-20 10:09:32 +0300

    added read and count methods

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(f"{num_words} was found in this file" )
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+    
+main()
