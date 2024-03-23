de7facc70660a4c114992a732778a80e4531dea8
jayy734 <angelsoenyunt@gmail.com>
2024-03-23 16:10:46 +0300

    added a method that return sorted list of alphabets and finished the projects

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     num_letters = count_letter(text)
-    print(f"{num_letters} was found in this file" )
+    chars_sorted_list = chars_dict_sorted_list(num_letters)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,6 +34,18 @@ def count_letter(text):
         else :
             char[lowered] = 1
     return char
+
+def sort_on(d):
+    return d["num"]
+
+def  chars_dict_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char":ch , "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True , key = sort_on)
+    return sorted_list
+
+
 
     
 main()
